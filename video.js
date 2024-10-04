f756c6f5e46567b88802b92420f40db643546316
DilrubaShondha <dilrubaeasmin2867@gmail.com>
2024-10-04 21:27:40 +0600

    some updates commit

--- video.js
+++ video.js
@@ -21,46 +21,29 @@ const loadVideos = () => {
     .then((data) => displayVideos(data.videos))
     .catch((error) => console.log(error))
 };
+const loadCategoryVideos = (id) =>{
+    fetch (`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.category))
+    .catch((error) => console.log(error))
+};
 //create display categories
 const displayCatergories = (categories) =>{
     const categoryContainer =document.getElementById('categories');
     categories.forEach((item) => {
     // console.log(item)
  //create a button
- const button = document.createElement("button");
- button.classList = "btn"
- button.innerText = item.category;
+ const buttonContainer = document.createElement("div");
+ buttonContainer.innerHTML = `<button id ="btn-${item.category_id}" onclick="loadCategoryVideos(${item.category_id})" class="btn category-btn">${item.category}</button>`;
  //add button
- categoryContainer.append(button);
+ categoryContainer.append(buttonContainer);
 });
 };
-//create display videos
-// const cardDemo = {
-//         "category_id": "1003",
-//         "video_id": "aaaf",
-//         "thumbnail": "https://i.ibb.co/5LRQkKF/stick-and-stones.jpg",
-//         "title": "Sticks & Stones",
-//         "authors": [
-//             {
-//                 "profile_picture": "https://i.ibb.co/rdTZrCM/dev.jpg",
-//                 "profile_name": "Dave Chappelle",
-//                 "verified": true
-//             }
-//         ],
-//         "others": {
-//             "views": "113K",
-//             "posted_date": ""
-//         },
-//         "description": "Dave Chappelle's 'Sticks & Stones' 
-//         has garnered 113K views and remains a controversial yet highly
-//          engaging piece of stand-up comedy. Known for his fearless approach, 
-//          Dave dives into a wide range of topics, delivering his unique perspective
-//           with wit and sharp humor. As a verified artist, Dave's comedy is raw, honest, and unapologetically funny."
-// }
+
 const displayVideos = (videos) =>{
     const videoContainer =document.getElementById('videos');
+    videoContainer.innerHTML = "";
     videos.forEach((video) => {
- console.log(video)
  const card =document.createElement("div")
  card.classList = 'card card-compact';
  card.innerHTML=
